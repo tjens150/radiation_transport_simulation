6c9dae8077c6c377e58a3a54749cff2443317351
Trey Jensen <Acolyte@10-17-17-103.dynapool.wireless.nyu.edu>
2019-11-21 14:29:54 -0500

    Modified pulla for rad and mat dom, made ardep and rad trans into one program

--- pulla.pyx
+++ pulla.pyx
@@ -1,17 +1,20 @@
 from libc.stdlib cimport srand, rand, RAND_MAX
 
-cdef extern from 'math.h':
+cdef extern from "math.h":
     double log(double x)
 
-def pulla(double astep, double Eistep, double zfinal, double ptmpco, double me, double sigT,int seed):
+cdef extern from "math.h":
+    double sqrt(double x)
+
+def pulla(double astep, double Eistep, double zfinal, double ptmpco, double aeq, double me, double sigT,int seed):
     cdef double dlna, ptmp, ppull, pxstep
-    ptmp=ptmpco/astep**(3/2.)*(3/8.)*sigT*me/(Eistep*Eistep)*(2*(Eistep**3 + 9*Eistep*Eistep*me+8*Eistep*me*me+2*me**3)/((2*Eistep+me)*(2*Eistep+me))+(Eistep-2*me-2*me*me/Eistep)*log(1+2*Eistep/me))
+    ptmp=ptmpco/sqrt(astep*astep*astep*(1+aeq/astep))*(3/8.)*sigT*me/(Eistep*Eistep)*(2*(Eistep**3 + 9*Eistep*Eistep*me+8*Eistep*me*me+2*me**3)/((2*Eistep+me)*(2*Eistep+me))+(Eistep-2*me-2*me*me/Eistep)*log(1+2*Eistep/me))
     dlna=min(0.001/ptmp,0.001)
     srand(seed)
     ppull = float(rand())/(float(RAND_MAX))
     pxstep=ptmp*dlna
-    while pxstep < ppull:
-        ppull = float(rand())/(float(RAND_MAX))
+    while pxstep< ppull:
+        ppull=float(rand())/(float(RAND_MAX))
         Eistep=Eistep*astep/(astep*(1+dlna))
         astep=astep*(1+dlna)
         if 1/astep-1 < zfinal:
@@ -20,4 +23,3 @@ def pulla(double astep, double Eistep, double zfinal, double ptmpco, double me, 
         dlna=min(0.001/ptmp,0.001)
         pxstep=ptmp*dlna
     return astep, Eistep
-        
